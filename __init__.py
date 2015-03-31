c56fea438d23f21fd7293bfb5ef3538dbc04d879
blissland <blissland.house@googlemail.com>
2015-03-31 13:38:00 +0000

    Support for subtitles

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import chanutils.torrent
 from chanutils import get_doc, get_json, select_all, select_one, get_attr
 from chanutils import get_text, get_text_content, replace_entity, byte_size
+from chanutils import movie_title_year
 from playitem import TorrentPlayItem, PlayItemList
 
 _SEARCH_URL = "http://kickass.to/json.php"
@@ -42,6 +43,9 @@ def feed(idx):
     cat = _FEEDLIST[idx]['title']
     if cat.endswith('Music'):
       img = '/img/icons/music.svg'
+    subs = None
+    if cat == 'Movies':
+      subs = movie_title_year(title)
     el = select_one(l, "td.nobr")
     size = get_text_content(el)
     el = select_one(l, "td.green")
@@ -49,7 +53,7 @@ def feed(idx):
     el = select_one(l, "td.red")
     peers = get_text(el)
     subtitle = chanutils.torrent.subtitle(size, seeds, peers)
-    results.add(TorrentPlayItem(title, img, url, subtitle))
+    results.add(TorrentPlayItem(title, img, url, subtitle, subs=subs))
   return results
 
 def search(q):
@@ -63,11 +67,14 @@ def search(q):
     if not (cat in _CAT_WHITELIST):
       continue
     title = replace_entity(r['title'])
+    subs = None
+    if cat == 'Movies':
+      subs = movie_title_year(title)
     img = '/img/icons/film.svg'
     if cat == 'Music':
       img = '/img/icons/music.svg'
     size = byte_size(r['size'])
     subtitle = chanutils.torrent.subtitle(size, r['seeds'], r['peers'])
     url = r['torrentLink']
-    results.add(TorrentPlayItem(title, img, url, subtitle))
+    results.add(TorrentPlayItem(title, img, url, subtitle, subs=subs))
   return results
